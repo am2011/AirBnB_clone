7ee3cfa48a9cb0beb4a05d99522125a247bd9b85
am2011 <adel.meme.philo@gmail.com>
2022-11-24 16:05:29 -0800

    added storage instance for FileStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python3
+""" create a unique storage instance to FileStorage """
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
+
+
 __all__ = ["BaseModel"]
